69510bd71799106a5ccbe0968fa720d816c3ec89
yannick2009 <yannickkouakou13@gmail.com>
2025-01-20 08:46:56 +0100

    feat: initialize game by clearing terminal output

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "github.com/go-terminal-OX/internal"
+import (
+	"github.com/go-terminal-OX/internal"
+	"github.com/go-terminal-OX/pkg/utils"
+)
+
+func init() {
+	// Clear the terminal
+	utils.ClearStdout(0)
+}
 
 func main() {
+	// Start the game
 	internal.InitGame()
 }
